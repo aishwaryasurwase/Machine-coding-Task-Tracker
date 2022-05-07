961892a1d4c15b1a813014c7bb77cc6e96b03880
Aishwarya.Surwase <Aishwarya.Surwase@go-mmt.com>
2022-05-07 19:50:53 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
+import AddTask from './components/AddTask/AddTask';
+import Header from './components/Header/Header'
+import Tasks from './components/Tasks/Tasks';
+
+const tasks = [
+  { id: 1, name: 'complete udemy course', dayTime: '1st jan 2021, Thursday', reminder: false },
+  { id: 2, name: 'Make a resume', dayTime: '1st jan 2021, Thursday', reminder: true },
+  { id: 3, name: 'Prepare for low level design', dayTime: '1st jan 2021, Thursday', reminder: true },
+  { id: 4, name: 'Solve DS problems', dayTime: '1st jan 2021, Thursday', reminder: false }
+]
 
 function App() {
+  const [addTaskShow, setAddTaskShow] = useState(false);
+  const [tasksList, setTasksList] = useState(tasks);
+
+  const saveTask = (task) => {
+    if (!task.name || !task.dayTime) {
+      return;
+    }
+    task.id = Math.floor(Math.random() * 40);
+    setTasksList([...tasksList, task])
+  }
+
+  const addTaskClicked = () => {
+    setAddTaskShow(!addTaskShow)
+  }
+
+  const clickToggleTask = (id) => {
+    const updatedTasks = tasksList.map((task) => task.id === id ? { ...task, reminder : !task.reminder } : task);
+    setTasksList([...updatedTasks])
+  }
+
+  const onDeleteTask = (id) => {
+    const updatedTasksLst = tasksList.filter((task) => task.id !== id);
+    setTasksList(updatedTasksLst)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <Header addTaskClicked={addTaskClicked}/>
+      {addTaskShow && <AddTask saveTask={saveTask} />}
+      {tasksList.length > 0 ? <Tasks tasks={tasksList} toggleTask={clickToggleTask} onDeleteTask={onDeleteTask}/>: <h3>No Tasks Available</h3> }
     </div>
   );
 }
